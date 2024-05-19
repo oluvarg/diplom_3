e542a679b0a29b66e1fd27ff3ede3dcf4f9f5e2c
oluvarg <156815645+oluvarg@users.noreply.github.com>
2024-05-19 11:24:58 +0300

    Revert "diplom_3"

--- data.py
+++
@@ -1,8 +0,0 @@
-class UrlsData:
-    REGISTER = 'https://stellarburgers.nomoreparties.site/register'
-    LOGIN = 'https://stellarburgers.nomoreparties.site/login'
-    FORGOT_PASSWORD = 'https://stellarburgers.nomoreparties.site/forgot-password'
-    MAIN_PAGE = 'https://stellarburgers.nomoreparties.site'
-    FEED_PAGE = 'https://stellarburgers.nomoreparties.site/feed'
-    ACCOUNT = 'https://stellarburgers.nomoreparties.site/account'
-    HISTORY_PAGE = 'https://stellarburgers.nomoreparties.site/account/order-history'
